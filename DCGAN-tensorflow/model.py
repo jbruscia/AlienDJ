70262e8b9d19e19460d4a7988d5702a1100157b5
Jasmine Walker <jasminewalker@jasmines-mbp.dhcp.nd.edu>
2017-11-27 18:02:35 -0500

    its running. jasmine

--- DCGAN-tensorflow/model.py
+++ DCGAN-tensorflow/model.py
@@ -15,9 +15,9 @@ def conv_out_size_same(size, stride):
 
 class DCGAN(object):
   def __init__(self, sess, input_height=128, input_width=128, crop=True,
-         batch_size=64, sample_num = 64, output_height=1024, output_width=1024,
+         batch_size=64, sample_num = 64, output_height=64, output_width=64,
          y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
-         gfc_dim=1024, dfc_dim=1024, c_dim=2, dataset_name='default',
+         gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
          input_fname_pattern='*.wav', checkpoint_dir=None, sample_dir=None):
     """
 
@@ -75,7 +75,7 @@ class DCGAN(object):
       self.c_dim = self.data_X[0].shape[-1]
     else:
       self.data = glob(os.path.join("./data", self.dataset_name, self.input_fname_pattern))
-    self.c_dim = 2
+    self.c_dim = c_dim
 
     self.grayscale = (self.c_dim == 1)
 
